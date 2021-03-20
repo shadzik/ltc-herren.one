0165fa9743316699f7e70e10a4cd5aea3fa4b672
Bartosz Świątek <shadzik@gmail.com>
2021-03-20 23:35:26 +0100

    Add contact page

--- makeSite.sh
+++ makeSite.sh
@@ -1,2 +1,3 @@
 cat header.html table.html footer.html > index.html
 cat header.html kalendar-content.html footer.html > kalendar.html
+cat header.html kontakt-content.html footer.html > kontakt.html
