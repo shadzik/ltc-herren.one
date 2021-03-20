4230e63fa0792f298aa0fb7a11998f2116bbff5a
Bartosz Świątek <shadzik@gmail.com>
2021-03-20 21:23:25 +0100

    init

---
+++ herren1.js
@@ -0,0 +1,40 @@
+const { doesNotMatch } = require('assert');
+const puppeteer = require('puppeteer')
+
+async function scrapeTable(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const el = await page.$x('//*[@id="content-row2"]/h1');
+  const title = await page.evaluate(el => el.textContent, el[0]);
+  console.log("<div class='container'>");
+  console.log("<h3>"+title+"</h3>");
+
+  const xpaths = ['//*[@id="content-row2"]/table[1]', '//*[@id="content-row2"]/table[2]'];
+  for (let xpath of xpaths) {
+	  const table = await page.$x(xpath);
+    const changeClass = await page.evaluate(() => {
+      const anchors = Array.from(document.querySelectorAll('table'));
+      anchors.map(table => table.setAttribute('class', "table"));
+      const tables = anchors.map(table => table.getAttribute('class'));
+	    return tables;
+    })
+	  const data = await page.evaluate(() => {
+	    const anchors = Array.from(document.querySelectorAll('table tbody tr td a'));
+	    anchors.map(td => td.setAttribute('href', "https://tvbb.liga.nu" + td.getAttribute('href')));
+	    const links = anchors.map(td => td.getAttribute('href'));
+	    return links;
+	  })
+	  const result = await page.evaluate(el => el.outerHTML, table[0]);
+	  console.log(result);
+  }
+
+  browser.close();
+  console.log("</div>");
+}
+
+
+scrapeTable('https://tvbb.liga.nu/cgi-bin/WebObjects/nuLigaTENDE.woa/wa/groupPage?championship=TVBB+Sommer+2021&group=1635442');
+//scrapeTable('https://tvbb.liga.nu/cgi-bin/WebObjects/nuLigaTENDE.woa/wa/groupPage?targetFed=TVBB&championship=TVBB+Sommer+2021&group=1609669')
+//scrapeTable('https://tvbb.liga.nu/cgi-bin/WebObjects/nuLigaTENDE.woa/wa/groupPage?championship=TVBB+Sommer+2021&group=1623544')
---
+++ makeSite.sh
@@ -0,0 +1,2 @@
+cat header.html table.html footer.html > index.html
+cat header.html kalendar-content.html footer.html > kalendar.html
