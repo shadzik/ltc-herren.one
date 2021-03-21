484221c9ed5794d6eac9bd348fb27e86de102af0
Bartosz Świątek <shadzik@gmail.com>
2021-03-21 11:48:45 +0100

    Add JS to dynamically change active state of a menu item

--- makeSite.sh
+++ makeSite.sh
@@ -1,3 +1,5 @@
 cat header.html table.html footer.html > index.html
 cat header.html kalendar-content.html footer.html > kalendar.html
 cat header.html kontakt-content.html footer.html > kontakt.html
+cat header.html 2019.html footer.html > table-2019.html
+cat header.html 2020.html footer.html > table-2020.html
