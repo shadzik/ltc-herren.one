8a76bfed2863a04236a559ba95b479cbc04e61c7
Bartosz Świątek <shadzik@gmail.com>
2021-04-10 00:59:11 +0200

    Fix typo

--- makeSite.sh
+++ makeSite.sh
@@ -1,9 +1,9 @@
 cat header.html table.html footer.html > index.html
 
 source /home/users/ltcherren/venv/bin/activate
-python3 ltc-calendar.py > kalendar-content.html
+python3 ltc-calendar.py > kalender-content.html
 
-cat header.html kalendar-content.html footer.html > kalendar.html
+cat header.html kalender-content.html footer.html > kalender.html
 cat header.html kontakt-content.html footer.html > kontakt.html
 cat header.html 2019.html footer.html > table-2019.html
 cat header.html 2020.html footer.html > table-2020.html
