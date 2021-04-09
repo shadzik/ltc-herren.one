2f81f50591f09e242ff0584abca1e25b36c1309a
Bartosz Świątek <shadzik@gmail.com>
2021-04-09 19:07:55 +0200

    Update makeSite.sh

--- makeSite.sh
+++ makeSite.sh
@@ -1,4 +1,8 @@
 cat header.html table.html footer.html > index.html
+
+source /home/users/ltcherren/venv/bin/activate
+python3 ltc-calendar.py > kalendar-content.html
+
 cat header.html kalendar-content.html footer.html > kalendar.html
 cat header.html kontakt-content.html footer.html > kontakt.html
 cat header.html 2019.html footer.html > table-2019.html
